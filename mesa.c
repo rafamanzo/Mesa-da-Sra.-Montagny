00c52a7d5122f9d46917961ab1caf6e5e9d71321
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2011-03-06 14:39:15 -0300

    Método recursivo de verificação
    gitignore atualizado

--- mesa.c
+++ mesa.c
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+#include<stdio.h>
 #include "ed/vertex.h"
 #include "ed/digraph.h"
 #include "ed/graph.h"
@@ -6,46 +7,66 @@
 #include "mesa.h"
 
 void solver(){
-  int instance;
+  int instance, i;
+  int *side;
   Graph G;
 
   instance = 1;
   G = inputInstance();
   while(G != NULL){
+    /*printf("\nAqui G->V=%d", G->V);*/
     outInstance(instance, processInstance(G));
     instance++;
-    DIGRAPHdestroy(G);
+    GRAPHdestroy(G);
     G = inputInstance();
   }
 }
 
 int processInstance(Graph G){
-  int i, j, k;
-  int *side; /*array para marcar os lados da mesa*/
+  int *flag, *side, i, j;
 
-  side = malloc((G->V + 1)*sizeof(int));
-
-  for(i = 1; i <= G->V; i++){
+  side = malloc((G->V)*sizeof(int));
+  flag = malloc((G->V)*sizeof(int));
+  for(i = 0; i < G->V; i++){
     side[i] = STANDING;
+    flag[i] = 0;
   }
 
-  for(i = 1; i <= G->V; i++){
-    if(side[i] == STANDING){
-      side[i] = LEFT;
+  for(i = 1; i < G->V; i++){
+    if(!recursiveCheck(G, i, side, flag)){
+      return 0;
     }
-    for(j = i + 1; j < G->V; j++){
-      if(G->adj[i][j] == 1){
-        k = (side[i] % 2) + 1;
-        if((side[i] == k) | side[i] == side[j]){
-          return 0;
-        }else{
-          side[j] = k;
-        }
+    for(j = 1; j < G->V; j++){
+      if(!flag[j]){
+        i = j;
+        j = G->V;
       }
     }
   }
 
-  free(side);
+  return 1;
+
+}
+
+int recursiveCheck(Graph G, Vertex actual, int *side, int *flag){
+  int i;
+
+  flag[actual] = 1;
+
+  if(side[actual] == STANDING){
+    side[actual] = LEFT;
+  }
+
+  for(i = 1; i < G->V; i++){
+    if(G->adj[actual][i]){
+      if(side[i] == side[actual]){
+        return 0;
+      }else if(side[i] == STANDING){
+        side[i] = (side[actual] % 2) + 1;
+        return recursiveCheck(G, i, side, flag);
+      }
+    }
+  }
 
   return 1;
 }
--- mesa.h
+++ mesa.h
@@ -2,5 +2,6 @@
 #define RIGHT 1
 #define LEFT 2
 
+int recursiveCheck(Graph G, Vertex actual, int *side, int *flag);
 int processInstance(Graph G);
 void solver();
